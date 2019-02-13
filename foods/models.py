f12921c303208e9f52cf9ce4aa9d7d1e8e09bfe0
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2019-02-13 11:58:26 +0700

    Created models and used admin mode

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    restaurant_name = models.CharField(max_length=200)
+    location = models.TextField(null=True)
+    contact = models.TextField(null=True)
+    def __str__(self):
+        return self.restaurant_name
+
+class Review(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    review_text = models.TextField(null=True)
+    username = models.CharField(max_length=100)
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    menu_text = models.CharField(max_length=100)
+    cost = models.IntegerField(default=0)
+    def __str__(self):
+        return self.menu_text
