8e57a81ac715940376383e13cd0f1fe578a0a6c2
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2019-02-13 20:09:01 +0700

    Added result template to show restaurant lists

--- foods/views.py
+++ foods/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render, HttpResponse
 from .models import Restaurant, Category, Menu, Review
 
 def index(request):
-    restaurant_list = Restaurant.objects.all()
-    return render(request,'foods/index.html',{'restaurant_list':restaurant_list})
+    category_list = Category.objects.all()
+    return render(request,'foods/index.html',{'category_list':category_list})
+
+def result(request):
+    #print(request.POST['category'])
+    restaurant_list = Restaurant.objects.filter(category__category_text=request.POST['category']) 
+    for i in restaurant_list:
+        print(i.restaurant_text)
+    return render(request,'foods/result.html',{'restaurant_list':restaurant_list})
